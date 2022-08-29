8544650ba99e83207ae93a49ac9fa958204bcd9e
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2022-08-29 16:04:43 +0100

    dont need normal for tris outside view

--- pyrasterize-fill.py
+++ pyrasterize-fill.py
@@ -132,6 +132,10 @@ def getVisibleTris(tris, worldVerts):
         v0 = worldVerts[tri[0]]
         v1 = worldVerts[tri[1]]
         v2 = worldVerts[tri[2]]
+        if (v0[2] >= NEAR_CLIP_PLANE or v1[2] >= NEAR_CLIP_PLANE or v2[2] >= NEAR_CLIP_PLANE
+          or v0[2] <= FAR_CLIP_PLANE or v1[2] <= FAR_CLIP_PLANE or v1[2] <= FAR_CLIP_PLANE):
+            normals.append((0,0,0))
+            continue
         # normal = crossProduct(subVec(v1, v0), subVec(v2, v0))
         sub10 = (v1[0] - v0[0], v1[1] - v0[1], v1[2] - v0[2])
         sub20 = (v2[0] - v0[0], v2[1] - v0[1], v2[2] - v0[2])
@@ -139,9 +143,6 @@ def getVisibleTris(tris, worldVerts):
             sub10[2]*sub20[0] - sub10[0]*sub20[2],
             sub10[0]*sub20[1] - sub10[1]*sub20[0])
         normals.append(normal)
-        if (v0[2] >= NEAR_CLIP_PLANE or v1[2] >= NEAR_CLIP_PLANE or v2[2] >= NEAR_CLIP_PLANE
-            or v0[2] <= FAR_CLIP_PLANE or v1[2] <= FAR_CLIP_PLANE or v1[2] <= FAR_CLIP_PLANE):
-            continue
         # isVisible = (dotProduct(v0, normal) < 0)
         isVisible = ((v0[0]*normal[0] + v0[1]*normal[1] + v0[2]*normal[2]) < 0)
         if isVisible:
