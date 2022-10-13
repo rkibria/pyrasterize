5021939f37166499a075215a0d5374f5cd28d67e
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2022-10-13 22:55:49 +0100

    f

--- test_frustum.py
+++ test_frustum.py
@@ -6,6 +6,14 @@
 import math
 import vecmat
 
+def perspective_projection(v_hom, view_plane):
+    """Projection"""
+    return (view_plane * v_hom[0] / -v_hom[2], view_plane * v_hom[1] / -v_hom[2])
+
+def get_view_plane_from_fov(fov):
+    """Return view plane distance"""
+    return 1 / math.tan(vecmat.deg_to_rad(fov / 2))
+
 def test_project_point():
     """???"""
     world_points = [
@@ -15,10 +23,8 @@ def test_project_point():
         [-1, 0, -1, 1],
         [-2, 0, -2, 1],
         ]
-    fov = 90
-    view_plane = 1 / math.tan(vecmat.deg_to_rad(fov / 2))
 
+    view_plane = get_view_plane_from_fov(90)
     for v_hom in world_points:
-        proj_point = (view_plane * v_hom[0] / -v_hom[2], view_plane * v_hom[1] / -v_hom[2])
+        proj_point = perspective_projection(v_hom, view_plane)
         print(v_hom, proj_point)
-
