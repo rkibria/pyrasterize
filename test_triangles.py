7af80c1f82f51ea741ad88ad57b0c71d6507a879
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2023-02-26 19:16:45 +0000

    implement flat top

--- test_triangles.py
+++ test_triangles.py
@@ -27,23 +27,66 @@ class MockPixelArray:
         text = "\n".join(lines)
         return "\n" + text + "\n"
 
-def draw_triangle_2d(array, color, x0, y0, x1, y1, x2, y2):
+
+def _draw_flat_top_triangle_2d(array, color, x1, y1, x2, y2, x3, y3):
     """
-    Draw a triangle in 2d coordinates.
-    The array object is accessed with a[x,y]=... syntax
+    Draw flat top triangle
     """
-    # P2 is LEFT of peak, P1 is RIGHT of peak
-    dxy_left = (x2 - x0) / (y2 - y0)
-    dxy_right = (x1 - x0) / (y1 - y0)
-    xs = x0
-    xe = x0
-    for y in range(int(y0), int(y1) + 1):
-        # Draw line
+    if x2 < x1:
+        x2, x1 = x1, x2
+    
+    height = y3 - y1
+
+    dx_left  = (x3 - x1) / height
+    dx_right = (x3 - x2) / height
+
+    xs = x1
+    xe = x2 + 0.5
+
+    for y in range(int(y1), int(y3 + 1)):
         for x in range(int(xs), int(xe) + 1):
             array[x, y] = color
-        # Go down scanline
-        xs += dxy_left
-        xe += dxy_right
+        xs += dx_left
+        xe += dx_right
+
+
+def _draw_flat_bottom_triangle_2d(array, color, x1, y1, x2, y2, x3, y3):
+    """
+    Draw flat bottom triangle
+    """
+    pass
+
+def draw_triangle_2d(array, color, x1, y1, x2, y2, x3, y3):
+    """
+    Draw a triangle in 2d coordinates by decomposing it into separate flat-top/flat-bottom triangles.
+    Writes output pixels with array[x,y]=color.
+    Positive y is down.
+    """
+    if (x1 == x2 and x2 == x3) or (y1 == y2 and y2 == y3):
+        return
+
+    if y2 < y1:
+        x2, x1 = x1, x2
+        y2, y1 = y1, y2
+
+    if y3 < y1:
+        x3, x1 = x1, x3
+        y3, y1 = y1, y3
+
+    if y3 < y2:
+        x3, x2 = x2, x3
+        y3, y2 = y2, y3
+
+    if y1 == y2:
+        _draw_flat_top_triangle_2d(array, color, x1, y1, x2, y2, x3, y3)
+    else:
+        if y2 == y3:
+            _draw_flat_bottom_triangle_2d(array, color, x1, y1, x2, y2, x3, y3)
+        else:
+            new_x = x1 + int(0.5 + (y2 - y1) * (x3 - x1) / (y3 - y1))
+            _draw_flat_bottom_triangle_2d(array, color, x1, y1, new_x, y2, x2, y2)
+            _draw_flat_top_triangle_2d(array, color, x2, y2, new_x, y2, x3, y3)
+
 
 # Positive y is down. peak, lower right, lower left
 TRIANGLE_FLAT_BOTTOM = [25, 20, 27, 22, 23, 22]
