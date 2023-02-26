81c7adf7e3cc760a4aec4eb3d0ce4c2c9dff3c7d
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2023-02-26 17:43:34 +0000

    failing test flat top

--- test_triangles.py
+++ test_triangles.py
@@ -48,7 +48,7 @@ def draw_triangle_2d(array, color, x0, y0, x1, y1, x2, y2):
 # Positive y is down. peak, lower right, lower left
 TRIANGLE_FLAT_BOTTOM = [25, 20, 27, 22, 23, 22]
 
-EXPECTED_FLAT_TOP_TRIANGLE= """
+EXPECTED_FLAT_BOTTOM_TRIANGLE= """
 .......
 ...x...
 ..xxx..
@@ -57,11 +57,30 @@ EXPECTED_FLAT_TOP_TRIANGLE= """
 """
 
 def test_draw_flat_bottom_triangle():
-    """A flat top triangle is drawn"""
+    """A flat bottom triangle is drawn"""
     array = MockPixelArray()
     draw_triangle_2d(array, TRIANGLE_COLOR, *TRIANGLE_FLAT_BOTTOM)
     actual = array.get_text(22, 19, 28, 23)
+    assert(EXPECTED_FLAT_BOTTOM_TRIANGLE == actual)
+
+
+TRIANGLE_FLAT_TOP = [23, 20, 27, 20, 25, 22]
+
+EXPECTED_FLAT_TOP_TRIANGLE= """
+.......
+.xxxxx.
+..xxx..
+...x...
+.......
+"""
+
+def test_draw_flat_top_triangle():
+    """A flat top triangle is drawn"""
+    array = MockPixelArray()
+    draw_triangle_2d(array, TRIANGLE_COLOR, *TRIANGLE_FLAT_TOP)
+    actual = array.get_text(22, 19, 28, 23)
     assert(EXPECTED_FLAT_TOP_TRIANGLE == actual)
+
 
 if __name__ == '__main__':
     test_draw_flat_bottom_triangle()
