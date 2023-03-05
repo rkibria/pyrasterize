62793bfee8870b8ef82e8eac95ce9060cfea9ef5
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2023-03-05 01:03:21 +0000

    demo models

--- demo_draw_modes.py
+++ demo_draw_modes.py
@@ -1,5 +1,5 @@
 """
-Demonstrates drawing a single, spinning cube
+Demonstrates drawing various geometry with different shading algorithms
 """
 
 import math
@@ -26,14 +26,6 @@ CAMERA = { "pos": [0,0,3], "rot": [0,0,0], "fov": 90, "ar": RASTER_SCR_WIDTH/RAS
 # Light comes from a right, top, and back direction (over the "right shoulder")
 LIGHTING = {"lightDir" : (1, 1, 1), "ambient": 0.3, "diffuse": 0.7}
 
-def create_scene_graph():
-    """Create the main scene graph"""
-    # The scene graph's top element is the "root" element which has no geometry of its own
-    scene_graph = { "root": rasterizer.get_model_instance(None) }
-    # The root has a single child which is the spinning cube (a default unit cube)
-    scene_graph["root"]["children"]["cube"] = rasterizer.get_model_instance(meshes.get_cube_mesh((255, 255, 255)))
-    return scene_graph
-
 def draw_scene_graph(surface, frame, scene_graph):
     """Draw and animate the scene graph"""
     # Set the transformation matrix of the root element to a combination of x/y/z rotations
@@ -54,49 +46,65 @@ def main_function():
 
     PYGAME_SCR_SIZE = (800, 600)
     screen = pygame.display.set_mode(PYGAME_SCR_SIZE)
-    pygame.display.set_caption("pyrasterize spinning cube demo")
+    pygame.display.set_caption("pyrasterize drawing modes demo")
     clock = pygame.time.Clock()
 
     pygame.mouse.set_cursor(*pygame.cursors.broken_x)
 
-    scene_graph = create_scene_graph()
-    cube_inst = scene_graph["root"]["children"]["cube"]
+    # The scene graph's top element is the "root" element which has no geometry of its own
+    scene_graph = { "root": rasterizer.get_model_instance(None) }
+
+    # The root has the various geometric objects as children
+    instances = [
+        ["Cube", rasterizer.get_model_instance(meshes.get_cube_mesh())],
+        ["Sphere", rasterizer.get_model_instance(meshes.get_sphere_mesh(1, 20, 10))],
+    ]
+    for name,model in instances:
+        scene_graph["root"]["children"][name] = model
+    cur_inst = 0
 
     font = pygame.font.Font(None, 30)
     TEXT_COLOR = (200, 200, 230)
-    title_1 = font.render("Left button toggles draw mode", True, TEXT_COLOR)
-
-    drawing_mode = 0
+    textblock_drawmode = font.render("", True, TEXT_COLOR)
+    textblock_model = font.render("", True, TEXT_COLOR)
 
     drawing_mode_names = ["Gouraud shading", "Flat shading", "Wireframe with backface culling", "Wireframe"]
+    drawing_mode = 0
 
-    def set_draw_mode_title():
-        nonlocal font
-        nonlocal drawing_mode
-        nonlocal title_1
-        title_1 = font.render(f"Draw mode (left button toggles): {drawing_mode_names[drawing_mode]}", True, TEXT_COLOR)
+    def regenerate_textblocks():
+        nonlocal textblock_drawmode
+        nonlocal textblock_model
+        textblock_drawmode = font.render(f"Draw mode (left button toggles): {drawing_mode_names[drawing_mode]}", True, TEXT_COLOR)
+        textblock_model = font.render(f"Model (right button toggles): {instances[cur_inst][0]}", True, TEXT_COLOR)
 
     def set_draw_mode():
         """Set the cube instance's drawing parameters according to current mode"""
         nonlocal drawing_mode
-        nonlocal cube_inst
-        cube_inst["gouraud"] = (drawing_mode == 0)
-        cube_inst["wireframe"] = (drawing_mode == 2 or drawing_mode == 3)
-        cube_inst["noCulling"] = (drawing_mode == 3)
+        nonlocal instances
+        nonlocal cur_inst
+        for i in range(len(instances)):
+            instances[i][1]["enabled"] = (i == cur_inst)
+        instances[cur_inst][1]["gouraud"] = (drawing_mode == 0)
+        instances[cur_inst][1]["wireframe"] = (drawing_mode == 2 or drawing_mode == 3)
+        instances[cur_inst][1]["noCulling"] = (drawing_mode == 3)
 
-    def on_left_down():
-        """Handle left button down"""
-        nonlocal drawing_mode
-        drawing_mode = drawing_mode + 1 if drawing_mode < 3 else 0
+    def on_mouse_button_down(event):
+        """Handle mouse button down"""
+        if event.button == 1:
+            nonlocal drawing_mode
+            drawing_mode = drawing_mode + 1 if drawing_mode < 3 else 0
+        elif event.button == 3:
+            nonlocal cur_inst
+            cur_inst = cur_inst + 1 if cur_inst < (len(instances) - 1) else 0
         set_draw_mode()
-        set_draw_mode_title()
+        regenerate_textblocks()
 
     set_draw_mode()
-    set_draw_mode_title()
+    regenerate_textblocks()
 
     frame = 0
     done = False
-    title_2 = font.render("", True, TEXT_COLOR)
+    textblock_fps = font.render("", True, TEXT_COLOR)
 
     offscreen = pygame.Surface(RASTER_SCR_SIZE)
 
@@ -106,19 +114,20 @@ def main_function():
             if event.type == pygame.QUIT:
                 done = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                on_left_down()
+                on_mouse_button_down(event)
 
         offscreen.fill(RGB_BLACK)
         draw_scene_graph(offscreen, frame, scene_graph)
 
         screen.blit(pygame.transform.scale(offscreen, PYGAME_SCR_SIZE), (0,0))
-        screen.blit(title_1, (30, 20))
-        screen.blit(title_2, (30, 50))
+        screen.blit(textblock_drawmode, (30, 20))
+        screen.blit(textblock_model, (30, 50))
+        screen.blit(textblock_fps, (30, 80))
 
         pygame.display.flip()
         frame += 1
         if frame % 30 == 0:
-            title_2 = font.render(f"{round(clock.get_fps(), 1)} fps", True, TEXT_COLOR)
+            textblock_fps = font.render(f"{round(clock.get_fps(), 1)} fps", True, TEXT_COLOR)
 
 if __name__ == '__main__':
     main_function()
