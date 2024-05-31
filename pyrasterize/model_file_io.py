458956c2b5756478a716f73617fca78df1ccaa04
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2024-05-31 13:58:35 +0100

    Refactor

--- pyrasterize/model_file_io.py
+++ pyrasterize/model_file_io.py
@@ -5,44 +5,39 @@
 Model file loading functions
 """
 
-import os, pathlib
+import os
 
-def get_model_from_obj_file(fname):
-    """Return model loaded from a Wavefront .obj file"""
-    with open(fname, encoding="utf-8") as file:
-        content = file.readlines()
-    content = [x.strip() for x in content]
+def parse_obj_file(obj_lines : list, mtl_lines : list) -> dict:
+    """
+    Get model from Wavefront .obj/.mtl
 
+    :param obj_lines: Stripped lines  with .obj contents
+    :param mtl_lines: Optional, Stripped lines with .mtl contents
+    :returns: Mesh dict
+    """
     mesh = {"verts": [], "tris": [], "colors": [], "uv": []}
     cur_color = (200, 200, 200)
     vts = []
     mtl_colors = {}
     uvs = {}
-    for line in content:
+
+    if mtl_lines is not None:
+        cur_mtl = None
+        for mtl_line in mtl_lines:
+            if mtl_line.startswith("newmtl "):
+                cur_mtl = mtl_line.split()[1]
+            elif mtl_line.startswith("Kd "):
+                tokens = mtl_line.split()[1:]
+                color = [int(float(tokens[i]) * 255) for i in range(3)]
+                mtl_colors[cur_mtl] = color
+
+    for line in obj_lines:
         if line.startswith("v "):
             tokens = line.split()
             mesh["verts"].append((float(tokens[1]), float(tokens[2]), float(tokens[3])))
         if line.startswith("vt "):
             tokens = line.split()
             vts.append((float(tokens[1]), float(tokens[2])))
-        if line.startswith("mtllib "):
-            tokens = line.split()[1:]
-            mtl_filename = tokens[0]
-            mtl_filename = os.path.join(os.path.dirname(os.path.abspath(fname)), mtl_filename)
-            try:
-                with open(mtl_filename, encoding="utf-8") as file:
-                    mtl_content = file.readlines()
-                mtl_content = [x.strip() for x in mtl_content]
-                cur_mtl = None
-                for mtl_line in mtl_content:
-                    if mtl_line.startswith("newmtl "):
-                        cur_mtl = mtl_line.split()[1]
-                    elif mtl_line.startswith("Kd "):
-                        tokens = mtl_line.split()[1:]
-                        color = [int(float(tokens[i]) * 255) for i in range(3)]
-                        mtl_colors[cur_mtl] = color
-            except:
-                pass
         elif line.startswith("usemtl "):
             mtl = line.split()[1]
             if mtl in mtl_colors:
@@ -74,5 +69,30 @@ def get_model_from_obj_file(fname):
                 mesh["uv"].append(uvs[i])
             else:
                 mesh["uv"].append((0.0, 0.0))
-    # print(f"--- loaded {fname}: {len(mesh['verts'])} vertices, {len(mesh['tris'])} triangles")
     return mesh
+
+def get_model_from_obj_file(fname : str) -> dict:
+    """
+    Load Wavefront .obj file, loads referenced .mtl file from same path if present
+
+    :param fname: String with .obj contents
+    :returns: Mesh dict
+    """
+    with open(fname, encoding="utf-8") as file:
+        obj_str = file.readlines()
+    obj_lines = [x.strip() for x in obj_str]
+    mtl_lines = None
+
+    for line in obj_lines:
+        if line.startswith("mtllib "):
+            tokens = line.split()[1:]
+            mtl_filename = tokens[0]
+            mtl_filename = os.path.join(os.path.dirname(os.path.abspath(fname)), mtl_filename)
+            try:
+                with open(mtl_filename, encoding="utf-8") as file:
+                    mtl_content = file.readlines()
+                mtl_lines = [x.strip() for x in mtl_content]
+            except:
+                pass
+
+    return parse_obj_file(obj_lines, mtl_lines)
