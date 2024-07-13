6867124688d28354f0df1cb94645b2da7c9ac528
Raihan Kibria <4503850+rkibria@users.noreply.github.com>
2024-07-13 23:15:30 +0100

    Refactor

--- pyrasterize/labyrinth.py
+++ pyrasterize/labyrinth.py
@@ -39,24 +39,21 @@ class Labyrinth:
                     tile_name = f"tile_{row}_{col}"
                     scene_graph_root_instance["children"][tile_name] = rasterizer.get_model_instance(None)
                     tile_inst = scene_graph_root_instance["children"][tile_name]
+                    tile_transl = vecmat.get_transl_m4(self.tile_size / 2 + self.tile_size * col,
+                                                       0,
+                                                       -self.tile_size / 2 + -self.tile_size * (self.rows - 1 - row))
                     tile_inst["children"]["floor"] = rasterizer.get_model_instance(floor_model,
-                        preproc_m4=floor_preproc_m4,
-                        xform_m4=vecmat.get_transl_m4(self.tile_size / 2 + self.tile_size * col,
-                                                      0,
-                                                      -self.tile_size / 2 + -self.tile_size * (self.rows - 1 - row)),
-                                                      create_bbox=False)
-
+                        preproc_m4=floor_preproc_m4, xform_m4=tile_transl, create_bbox=False)
                     tile_inst["children"]["ceiling"] = rasterizer.get_model_instance(ceil_model,
-                        preproc_m4=ceil_preproc_m4,
-                        xform_m4=vecmat.get_transl_m4(self.tile_size / 2 + self.tile_size * col,
-                                                      0,
-                                                      -self.tile_size / 2 + -self.tile_size * (self.rows - 1 - row)),
-                                                      create_bbox=False)
+                        preproc_m4=ceil_preproc_m4, xform_m4=tile_transl, create_bbox=False)
 
     def create_walls(self,
                      scene_graph_root_instance,
                      wall_model,
                      wall_preproc_m4):
+        """
+        Creates instances 
+        """
         wall_inst = rasterizer.get_model_instance(wall_model, preproc_m4=wall_preproc_m4, create_bbox=False)
         # Wall meshes are culled if not facing the camera.
         wall_inst["instance_normal"] = [0, 0, 1]
@@ -65,19 +62,19 @@ class Labyrinth:
         for row in range(self.rows):
             row_tiles = self.tiles[row]
             for col in range(self.cols):
-                cell_name = f"cell_{row}_{col}"
-                scene_graph_root_instance["children"][cell_name] = rasterizer.get_model_instance(None,
+                tile_name = f"tile_{row}_{col}"
+                scene_graph_root_instance["children"][tile_name] = rasterizer.get_model_instance(None,
                     xform_m4=vecmat.get_transl_m4(self.tile_size * col, 0,
                                                   -self.tile_size * (self.rows - 1 - row)))
-                tile_inst = scene_graph_root_instance["children"][cell_name]
+                tile_inst = scene_graph_root_instance["children"][tile_name]
 
                 wall_n = False
                 wall_s = False
                 wall_w = False
                 wall_e = False
 
-                cell = row_tiles[col]
-                if cell == "#":
+                tile = row_tiles[col]
+                if tile == "#":
                     if row != 0 and self.tiles[row - 1][col] != "#":
                         wall_n = True
                     if row != self.rows -1 and self.tiles[row + 1][col] != "#":
